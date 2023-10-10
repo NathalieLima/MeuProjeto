8fa0cc581b23e6de6b6330b3d22bc1cfd2150a2d
NathalieLima <nathalielima3@gmail.com>
2023-10-10 18:14:54 -0300

    Adicionando arquivo de nova feature

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void)
+{
+    int numero1 = 0;
+    int numero2 = 0;
+
+    // pedir dois números 
+    cout << "Digite um número inteiro: ";
+    cin >> numero1;
+    cout << "Digite outro número inteiro: ";
+    cin >> numero2;
+
+
+    return 0;
+}
