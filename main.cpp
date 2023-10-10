f80361dd094f279d4652c6a101fa3f44fc99f3a9
Gabriel <gabrielteles931@gmail.com>
2023-10-10 18:26:11 -0300

    Adicionando soma e subtração.

--- main.cpp
+++ main.cpp
@@ -12,7 +12,13 @@ int main (void)
     cin >> numero1;
     cout << "Digite outro número inteiro: ";
     cin >> numero2;
+    cout << endl;
 
+    cout << "A soma dos números é: " << numero1 + numero2 << endl;
+    cout << endl;
+
+    cout << "A subtração dos números é: " << numero1 - numero2 << endl;
+    cout << endl;
 
     return 0;
 }
